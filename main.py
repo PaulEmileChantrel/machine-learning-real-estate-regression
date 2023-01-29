0c250e4ddf148bf2e0e9fcfd21a603fa9313cd02
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-01-29 15:57:09 -0500

    cross validation grid

--- main.py
+++ main.py
@@ -202,6 +202,11 @@ def main():
     grid_search.fit(re_prepared,labels)
 
     print(grid_search.best_params_)
+    print(grid_search.best_estimator_)
+
+    cvres = grid_search.cv_results_
+    for mean_score,params in zip(cvres['mean_test_score'],cvres['params']):
+        print(np.sqrt(-mean_score),params)
       
     
 if __name__ == '__main__':
